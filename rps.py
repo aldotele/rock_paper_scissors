51410d713a432c5ae8c266f7459796e15b858c6e
aldotele <aldo.telese@hotmail.it>
2021-07-04 11:08:23 +0200

    clean entry point

--- rps.py
+++ rps.py
@@ -1,32 +1,18 @@
 from rps_game.play_rps import PlayGame
-from rps_game.player import Player
+from rps_game.helper.tables import show_challengers
 
 
 def start_game():
     game_type = PlayGame.choose_game_type()
     max_rounds = PlayGame.choose_max_rounds()
     if game_type == 1:
-        play_human_vs_cpu(game_type, max_rounds)
+        player_1, player_2 = PlayGame.play_human_vs_cpu()
+        print(f"your opponent: {player_2.name}")
+        print()
     else:
-        play_cpu_vs_cpu(game_type, max_rounds)
-
-
-def play_human_vs_cpu(game_type, max_rounds):
-    human_name = input("choose a name: ")
-    human_player = Player(human_name)
-    virtual_player = Player()
-    print(f"your opponent: {virtual_player.name}")
-    print()
-    PlayGame(human_player, virtual_player, game_type, max_rounds)
-
-
-def play_cpu_vs_cpu(game_type, max_rounds):
-    cpu_1 = Player()
-    cpu_2 = Player()
-    print()
-    print(f"{cpu_1.name} against {cpu_2.name}")
-    print()
-    PlayGame(cpu_1, cpu_2, game_type, max_rounds)
+        player_1, player_2 = PlayGame.play_cpu_vs_cpu()
+    show_challengers(player_1, player_2)
+    PlayGame(player_1, player_2, game_type, max_rounds)
 
 
 if __name__ == '__main__':
