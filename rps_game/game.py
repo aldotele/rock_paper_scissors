d4640f9861aa18473495564d2baa186e11b24f1c
aldotele <aldo.telese@hotmail.it>
2021-07-04 11:10:00 +0200

    tables prints with external module

--- rps_game/game.py
+++ rps_game/game.py
@@ -1,18 +1,16 @@
-from rps_game.player import Player
-
-
 class Game:
     types = {1: "human versus CPU", 2: "CPU vs CPU"}
 
     def __init__(self, player_1, player_2, amt_rounds=1):
-        # player_1 and player_2 will be two objects
+        # player_1 and player_2 are two instances of Player class
         self.player_1 = player_1
         self.player_2 = player_2
         self.amt_rounds = amt_rounds
-        self.rounds = []
+        self.played_rounds = []
         self.scoreboard = {player_1.name: 0, player_2.name: 0}
+        self.winner = ""
 
-    def round(self, first_player_choice, second_player_choice):
+    def play_round(self, first_player_choice, second_player_choice):
         if first_player_choice == second_player_choice:
             pass
         elif first_player_choice == 1 and second_player_choice == 2:
@@ -34,26 +32,15 @@ class Game:
             self.scoreboard[self.player_1.name] += 1
             return self.player_1
 
-    def save_round(self, round):  # round is an object
-        self.rounds.append(round)
-
-    @staticmethod
-    def bitter_end(first_player, second_player):
-        first_player = 0
-        second_player = 0
-        while first_player == 0 and second_player == 0:
-            pass
-        # here the winner will be the first one to beat the opponent
-        pass
-
-    def show_scoreboard(self):
-        for player in self.scoreboard:
-            print(f"{player}: {self.scoreboard[player]} points")
+    def save_round(self, current_round):  # round is an object
+        self.played_rounds.append(current_round)
 
     def declare_winner(self):
         if self.scoreboard[self.player_1.name] > self.scoreboard[self.player_2.name]:
-            return self.player_1
+            self.winner = self.player_1
+            return self.winner
         elif self.scoreboard[self.player_2.name] > self.scoreboard[self.player_1.name]:
-            return self.player_2
+            self.winner = self.player_2
+            return self.winner
         else:
             return None
---
+++ rps_game/helper/tables.py
@@ -0,0 +1,19 @@
+def show_challengers(player_1, player_2):
+    hor_stick_len = len(player_1.name) + len(player_2.name) + 8
+    hor_stick = "-" * hor_stick_len
+    print(hor_stick)
+    print(f"| {player_1.name} VS {player_2.name} |")
+    print(hor_stick)
+
+
+def show_pretty_scoreboard(game):
+    hor_stick = "-" * 27
+    first_column = "Player"
+    second_column = "Score"
+    print(hor_stick)
+    print('|  {:<15}{:^7} |'.format(first_column, second_column))
+    print(hor_stick)
+    print('|  {:<15}{:^7} |'.format(game.player_1.name, game.scoreboard[game.player_1.name]))
+    print('|  {:<15}{:^7} |'.format(game.player_2.name, game.scoreboard[game.player_2.name]))
+    print(hor_stick)
+
