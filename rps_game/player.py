76ab6aa3917188c615cd1122dd38de6957444315
aldotele <aldo.telese@hotmail.it>
2021-07-05 18:48:38 +0200

    name validation check

--- rps_game/player.py
+++ rps_game/player.py
@@ -1,24 +1,29 @@
-import random
 from rps_game.helper.funny_names import FunnyNameGenerator
+import re
 
 
 class Player:
     def __init__(self, name=""):
         if name:  # if a name is given, it is a human player
-            self.player_type = "human"
-            self.name = name
-        else:  # a random choice code is generated
+            if Player.is_name_valid(name):
+                self.player_type = "human"
+                self.name = name
+            else:
+                raise ValueError("Player name must have at least three characters with one letter")
+        else:  # a random name is generated
             self.player_type = "virtual"
             self.name = FunnyNameGenerator.compose_name()
 
-
-if __name__ == '__main__':
-    pass
-
-
-
-
-
+    @staticmethod
+    def is_name_valid(name):
+        if len(name) >= 3:
+            out = re.search("[a-zA-Z]", name)  # making sure the name has at least one letter
+            if out:
+                return True
+            else:
+                return False
+        else:
+            return False
 
 
 
